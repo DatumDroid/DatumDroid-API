41231ab3a11d3fa404864dd2cf9c728de5a6f883
Gautam Gupta <gautam.2011@gmail.com>
2011-10-23 02:32:28 +0530

    Initial Commit

---
+++ dd-sample-config.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Configuration File
+ *
+ * Add your own definitions and variables (api keys etc) and rename this file
+ * to `dd-config.php`
+ *
+ * @package DatumDroid_API
+ * @subpackage Config
+ */
+
+// Exit if accessed directly
+if ( !defined( 'DD_DIR' ) ) exit;
+/**
+ * API Keys
+ */
+$dd_api_keys = array( 'guardian' => '' );
+
+?>
