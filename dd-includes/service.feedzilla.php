c2f49766b637ca37b2d4c3e1adb1e013618594d0
Gautam Gupta <gautam.2011@gmail.com>
2011-10-28 15:16:56 +0530

    Committing the file

--- dd-includes/service.feedzilla.php
+++ dd-includes/service.feedzilla.php
@@ -26,6 +26,28 @@ class DD_Service_Feedzilla extends DD_Search_Service {
 	var $max = '100';
 
 	/**
+	 * Get the required number of results
+	 *
+	 * Feedzilla, at max, can only return 100 results for a search query and it
+	 * also doesn't support any page parameters so we've to work accordingly
+	 *
+	 * You might still not be able to get the correct number of results
+	 *
+	 * @param bool $reset_query Reset query?
+	 * @return array Array of results
+	 */
+	function search( $reset_query = false ) {
+		if ( $this->rpp * $this->page > $this->max )
+			return array( "no results found" );
+
+		$this->pre_trim = ( $this->page - 1 ) * $this->rpp;
+		$this->rpp      = $this->rpp * $this->page;
+		$this->page     = 1;
+
+		return parent::search();
+	}
+
+	/**
 	 * Build and perform the query, return the results.
 	 * @param $reset_query boolean optional.
 	 * @return object
@@ -33,16 +55,17 @@ class DD_Service_Feedzilla extends DD_Search_Service {
 	function results( $reset_query = true ) {
 		$request  = 'http://api.feedzilla.com/v' . $this->api_ver . '/articles/search.' . $this->type;
 		$request .= '?q=' . urlencode( $this->query );
-		$request .= '&order=date&title_only=1';
+		$request .= '&order=date&title_only=1&client_source=' . DD_NAME . DD_VER;
 
 		if ( isset( $this->rpp ) ) {
-			// @todo Feedzilla bug, displays 1 result less than the requested
-			$request .= '&count=' . ( $this->rpp + 1 );
+			$request .= '&count=' . $this->rpp;
 		}
 
+		/*
 		if ( isset( $this->page ) ) {
 			$request .= '&page=' . $this->page;
 		}
+		*/
 
 		if ( isset( $this->lang ) ) {
 			//$request .= '&culture_code=' . $this->lang . '_us';
@@ -57,7 +80,9 @@ class DD_Service_Feedzilla extends DD_Search_Service {
 			$this->query = '';
 		}
 
-		return $this->objectify( $this->process( $request ) )->articles;
+		$results = $this->objectify( $this->process( $request ) );
+
+		return !empty( $results->articles ) ? $results->articles : false;
 	}
 
 }
